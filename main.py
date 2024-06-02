6c09221f918403d8736095b498689dda4d8081ac
Lola Amelia <amelialola465@gmail.com>
2024-06-02 19:25:37 +0700

    menambahkan status code 404

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
 # Import module 
 import requests
-import json
 import new_detect
 from time import sleep
 from servo import Servo
-# from buzzer import Buzzer
-# from waterpump import Waterpump
-import lcd
+from buzzer import Buzzer
+from lcd import LCD
+from datetime import datetime
+import cProfile 
 
 servo = Servo()
-# waterpump = Waterpump()
-# buzzer = Buzzer()
+buzzer = Buzzer()
+lcd = LCD()
 
 while(True):
-
     try:
-    #     print("getting status..")
-    #     x = requests.get('https://fishcounterta.000webhostapp.com/status.php')
-    #     response_json = x.json()
+        date = datetime.now()
+        print(f"{date} : getting status..")
+        response = requests.get('https://fishcounterta.000webhostapp.com/status.php')
+        if response.status_code == 404:
+            print("404 Not Found")
+            continue
 
-    #     # Mengekstrak nilai dari JSON
-    #     # id_value = response_json['id']
-    #     hitung_status = response_json['hitung']
+        response_json = response.json()
 
-    #     if hitung_status == 'true':
-    #         jumlah_value = int(response_json['jumlah'])
-    #         harga_value = int(response_json['harga'])
+        # Mengekstrak nilai dari JSON
+        # id_value = response_json['id']
+        hitung_status = response_json['hitung']
+
+        if hitung_status == 'true':
+            jumlah_value = int(response_json['jumlah'])
+            harga_value = int(response_json['harga'])
             
-
-            # waterpump.on()              # Hidupkan waterpump
-            jumlah_value = 5
-            harga_value = 100
             jumlah_ikan = 0
             while (jumlah_ikan < jumlah_value):
                 servo.close()             # Matikan servo
@@ -40,30 +40,23 @@ while(True):
             
             new_detect.stopProgram()
             print("Total ikan yang terdeteksi : {}".format(jumlah_ikan))
-            lcd.tampil(jumlah_value=jumlah_value,harga_value=harga_value*jumlah_value)
-            break
-            # waterpump.off()           # Matikan waterpump
-            # buzzer.setup_gpio()
-            # buzzer.hidup()            # Bunyikan buzzer
-            
-
-            # print(jumlah_value*harga_value) # Print harga total di terminal
+            buzzer.turn_on_buzzer()
+            lcd.tampil(jumlah_value=jumlah_value,harga_satuan=harga_value)
 
             # reset table status
-            # x = requests.post('https://fishcounterta.000webhostapp.com/status.php') 
-            # print(x.json())
+            x = requests.post('https://fishcounterta.000webhostapp.com/status.php') 
+            print(x.json())
 
     except KeyboardInterrupt:
-        # waterpump.off()
-        # servo.close()
-        # lcd.close()
+        servo.stop()
+        buzzer.stop()
         break
+    
+    except ConnectionError as errcon:
+        print(errcon.args)
 
     # except Exception as e:
-        # waterpump.off()
-        # servo.close()
-        # lcd.close()
-        # print(e.args)
+    #     print(e.args)
         
     finally:
         sleep(3)
