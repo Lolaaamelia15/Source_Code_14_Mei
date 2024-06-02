dc3ca6d4b835664ca13626ee3a268d2e744e292f
Lola Amelia <amelialola465@gmail.com>
2024-06-02 19:29:12 +0700

    membuat class LCD

--- lcd.py
+++ lcd.py
@@ -1,21 +1,22 @@
 from RPLCD import i2c
 from time import sleep 
 
-lcdmode = 'i2c'
-cols = 16
-rows = 2
-charmap = 'A00'
-i2c_expander = 'PCF8574'
+class LCD:
+    def __init__(self) -> None:
+        self.lcdmode = 'i2c'
+        self.cols = 16
+        self.rows = 2
+        self.charmap = 'A00'
+        self.i2c_expander = 'PCF8574'
+        self.address = 0x27
+        self.port = 1
+        self.lcd = i2c.CharLCD(self.i2c_expander, self.address, port=self.port, charmap=self.charmap, cols=self.cols, rows=self.rows)
 
-address = 0x27
-port = 1
+    def tampil(self, jumlah_value=0, harga_satuan=0):
+        self.lcd.write_string("Jumlah: {}\r\nHarga: Rp{}".format(jumlah_value, harga_satuan*jumlah_value))
+        sleep(15)
+        self.lcd.close(clear=True)
 
-lcd = i2c.CharLCD(i2c_expander, address, port=port, charmap=charmap, cols=cols, rows=rows)
-
-def tampil(jumlah_value=0, harga_value=0):
-    lcd.write_string("Jumlah: {} \r\nHarga: {}".format(jumlah_value, harga_value))
-    sleep(15)
-
-    lcd.close(clear=True)
-
-# tampil(20,100)
+if __name__ == "__main__":
+    lcd = LCD()
+    lcd.tampil(1000,5000)
